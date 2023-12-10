eb70e9550be953deced09ee74b97ab7df68466db
MK <mousamdreamer@gmail.com>
2023-12-10 22:50:13 +0530

    add new tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,21 @@
+const { JSDOM } = require('jsdom');
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = [];
+    const dom = new JSDOM(htmlBody);
+    const links = dom.window.document.querySelectorAll('a');
+    for (const link of links) {
+        if (link.href.slice(0, 1) === '/') {
+            // relative
+            urls.push(`${baseURL}${link.href}`);
+        }
+        else {
+            // absolute
+            urls.push(link.href);
+        }
+    }
+    return urls;
+}
 
 function normalizeURL(inputUrl) {
     const urlObj = new URL(inputUrl);
@@ -9,4 +27,4 @@ function normalizeURL(inputUrl) {
     return hostPath;
 }
 
-module.exports = { normalizeURL };
+module.exports = { normalizeURL, getURLsFromHTML };
