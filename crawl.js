3486efa256a3431e1261d2391b0c1f8587495d92
MK <mousamdreamer@gmail.com>
2023-12-10 21:06:05 +0530

    add new tests

--- crawl.js
+++ crawl.js
@@ -1,7 +1,12 @@
 
 function normalizeURL(inputUrl) {
     const urlObj = new URL(inputUrl);
-    return `${urlObj.hostname}${urlObj.pathname}`;
+    const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+
+    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
+        return hostPath.slice(0, -1);
+    }
+    return hostPath;
 }
 
 module.exports = { normalizeURL };
--- crawl.test.js
+++ crawl.test.js
@@ -1,8 +1,29 @@
 const { test, expect } = require('@jest/globals');
 const { normalizeURL } = require('./crawl');
 
-test('normalizeURL', () => {
+test('normalizeURL strip protocol', () => {
     const input = 'https://blog.boot.dev/path';
+    const actual = normalizeURL(input);
+    const expected = 'blog.boot.dev/path';
+    expect(actual).toEqual(expected);
+});
+
+test('normalizeURL strip trailing slash', () => {
+    const input = 'https://blog.boot.dev/path/';
+    const actual = normalizeURL(input);
+    const expected = 'blog.boot.dev/path';
+    expect(actual).toEqual(expected);
+});
+
+test('normalizeURL capitals', () => {
+    const input = 'https://BLog.boot.dev/path/';
+    const actual = normalizeURL(input);
+    const expected = 'blog.boot.dev/path';
+    expect(actual).toEqual(expected);
+});
+
+test('normalizeURL strip http', () => {
+    const input = 'http://blog.boot.dev/path/';
     const actual = normalizeURL(input);
     const expected = 'blog.boot.dev/path';
     expect(actual).toEqual(expected);
